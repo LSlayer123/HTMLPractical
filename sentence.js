6050f3a16737f687e579ea2cd6cb9a2db5b3019f
LSlayer123 <Lagombi01@gmail.com>
2021-11-12 15:35:40 +0000

    Fixed it poggies

--- sentence.js
+++ sentence.js
@@ -1,14 +1,16 @@
-let adjectives = ["fun", "happy", "sad", "painful", "horrific"]
-let nouns = ["cat", "dog", "mouse", "man", "lion", "sprout", "goldfish"]
-let verbs = ["eats", "sleeps", "wanders", "patrols", "stumbles", "drinks", "mopes"]
-let advebs = ["lazily", "guardedly", "dangerously", "sadly", "happily", "eagerly"]
+let adjectives = ["fun", "happy", "sad", "painful", "horrific"];
+let nouns = ["cat", "dog", "mouse", "man", "lion", "sprout", "goldfish"];
+let verbs = ["eats", "sleeps", "wanders", "patrols", "stumbles", "drinks", "mopes"];
+let adverbs = ["lazily", "guardedly", "dangerously", "sadly", "happily", "eagerly"];
 
 function randomInt(input){
-    return Math.floor(Math.random() * input)
+    return Math.floor(Math.random() * input);
 }
 
 function updateSentence(){
-    location = document.getElementById("sentenceLocation")
-    sentence = "The " + adjectives[randomInt(adjectives.length())] + " " + nouns[randomInt(nouns.length())] + " " + verbs[randomInt(verbs.length())] + " " + adverbs[randomInt(adverbs.length())]
-    location.innerHTML = sentence
+    let location = document.getElementById("sentenceLocation");
+    let sentence = "The " + adjectives[randomInt(adjectives.length)] + " " + nouns[randomInt(nouns.length)] + " " + verbs[randomInt(verbs.length)] + " " + adverbs[randomInt(adverbs.length)];
+    location.innerHTML = sentence;
 }       
+
+//document.getElementById("button").onclick = updateSentence;
